25016982d5f9f3a1d1b8e0b6d5f5dc33d31d2e44
captim <48884754+captim@users.noreply.github.com>
2019-11-21 14:47:15 +0200

    Problem with getAge(), get Birthday() and getPhone(String type)

--- src/ru/skillbench/tasks/text/ContactCardImpl.java
+++ src/ru/skillbench/tasks/text/ContactCardImpl.java
@@ -16,6 +16,7 @@ public class ContactCardImpl implements ContactCard{
     private int numOfTel = 0;
     @Override
     public ContactCard getInstance(Scanner scanner) {
+        ContactCard d = new ContactCardImpl();
         String temp;
         temp = scanner.nextLine();
         if(!temp.equals("BEGIN:VCARD"))
@@ -28,7 +29,7 @@ public class ContactCardImpl implements ContactCard{
         }
         String []FNX;
         FNX = temp.split("FN:");
-        FN = FNX[1];
+        ((ContactCardImpl) d).FN = FNX[1];
         //scanner.nextLine();
 
 
@@ -38,25 +39,26 @@ public class ContactCardImpl implements ContactCard{
         }
         String []ORGX;
         ORGX = temp.split("ORG:");
-        ORG = ORGX[1];
+        ((ContactCardImpl) d).ORG = ORGX[1];
 
         temp = scanner.nextLine();
         while(!temp.equals("END:VCARD")){
+            //temp = scanner.next();
             if(temp.startsWith("GENDER:")){
-//                if(GENDER != null || BDAY != null || TEL[0] != null){
-//                    throw new InputMismatchException();
-//                }
+                if(((ContactCardImpl) d).GENDER != null || ((ContactCardImpl) d).BDAY != null || ((ContactCardImpl) d).TEL[0] != null){
+                    throw new InputMismatchException();
+                }
                 String []GENDERX;
                 GENDERX = temp.split("GENDER:");
                 if(!GENDERX[1].equals("M") && !GENDERX[1].equals("F"))
                     throw new InputMismatchException();
-                GENDER = GENDERX[1];
+                ((ContactCardImpl) d).GENDER = GENDERX[1];
             }
             else if(temp.startsWith("BDAY:")){
-                if(TEL[0] != null) {
+                if(((ContactCardImpl) d).TEL[0] != null) {
                     throw new InputMismatchException();
                 }
-                BDAY = Calendar.getInstance();
+                ((ContactCardImpl) d).BDAY = Calendar.getInstance();
                 String []BDAYX;
                 String []BDAYX1;
                 BDAYX = temp.split("BDAY:");
@@ -69,29 +71,28 @@ public class ContactCardImpl implements ContactCard{
                 if(day > 31 || day < 1 || month > 12 || month < 1 || year > 9999 || year < 1000){
                     throw new InputMismatchException();
                 }
-                BDAY.set(year, month, day);
+                ((ContactCardImpl) d).BDAY.set(year, month, day);
             }
-            else if(temp.startsWith("TEL;")){
+            else if(temp.startsWith("TEL;TYPE=")){
                 String []Tel1;
                 Tel1 = temp.split("TEL;TYPE=");
                 String []Tel;
-                Tel = Tel1[1].split(",");
-                if(Tel.length != 2){
+                Tel = Tel1[1].split(":");
+                if(Tel.length != 2 || Tel[0].isEmpty()){
                     throw new InputMismatchException();
                 }
-                TEL[numOfTel] =  Tel[0];
-                Tel1 = Tel[1].split(":");
-                TEL[numOfTel + 1] = Tel1[1];
+                ((ContactCardImpl) d).TEL[numOfTel] =  Tel[0];
+                ((ContactCardImpl) d).TEL[numOfTel + 1] = Tel[1];
                 char[] data = new char[11];
-                data = TEL[numOfTel + 1].toCharArray();
-                if(TEL[numOfTel + 1].length() != 10)
+                data = ((ContactCardImpl) d).TEL[numOfTel + 1].toCharArray();
+                if(((ContactCardImpl) d).TEL[numOfTel + 1].length() != 10)
                     throw new InputMismatchException();
                 for (int i = 0; i < 10; i++) {
                     if(data[i] < '0' || data[i] > '9'){
                         throw new InputMismatchException();
                     }
                 }
-                numOfTel +=2;
+                numOfTel+=2;
             }
             else{
                 throw new InputMismatchException();
@@ -103,14 +104,14 @@ public class ContactCardImpl implements ContactCard{
         }
         if(!temp.equals("END:VCARD"))
             throw new InputMismatchException();
-        return this;
+        return d;
     }
 
     @Override
     public ContactCard getInstance(String data) {
         Scanner s = new Scanner(data);
-        getInstance(s);
-        return null;
+        ContactCard d = getInstance(s);
+        return d;
     }
 
     @Override
@@ -149,7 +150,10 @@ public class ContactCardImpl implements ContactCard{
         Calendar today = Calendar.getInstance();
         LocalDate newDate = LocalDate.of(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
         Period period = Period.between(oldDate, newDate);
-            return period;
+        if(today.get(Calendar.MONTH) < BDAY.get(Calendar.MONTH) || (today.get(Calendar.MONTH) == BDAY.get(Calendar.MONTH) && today.get(Calendar.DAY_OF_MONTH) <= BDAY.get(Calendar.DAY_OF_MONTH)))
+            period = period.minusDays(1);
+        period = period.plusMonths(1);
+        return period;
     }
 
     @Override
@@ -159,7 +163,7 @@ public class ContactCardImpl implements ContactCard{
         int age;
         Calendar today = Calendar.getInstance();
         age = today.get(Calendar.YEAR) - BDAY.get(Calendar.YEAR);
-        if(today.before(BDAY))
+        if(today.get(Calendar.MONTH) < BDAY.get(Calendar.MONTH) || (today.get(Calendar.MONTH) == BDAY.get(Calendar.MONTH) && today.get(Calendar.DAY_OF_MONTH) <= BDAY.get(Calendar.DAY_OF_MONTH)))
             age-= 1;
         return age;
     }
@@ -169,33 +173,14 @@ public class ContactCardImpl implements ContactCard{
         int i = 0;
         while(TEL[i] != null && !TEL[i].equals(type)){
             i += 2;
-            if(TEL[i] == null)
+            if(TEL[i + 1] == null)
                 throw new NoSuchElementException();
         }
-        String tel = TEL[i + 1];
-        char[] data = new char[14];
-        for (int j = 0; j < 14; j++) {
-            data[j] = '0';
-        }
-        char[] temp = tel.toCharArray();
-        for (int j = 0; j < 10; j++) {
-            data[j] = temp[j];
-        }
-        for (int j = 9; j >= 0; j--) {
-            data[j + 1] = data[j];
-        }
-        data[0] = '(';
-        for (int j = 10; j > 1; j--) {
-            data[j + 2] = data[j];
-        }
-        data[4] = ')';
-        data[5] = ' ';
-        for (int j = 12; j >= 9; j--) {
-            data[j + 1] = data[j];
-        }
-        data[9] = '-';
-        String NewTEL;
-        NewTEL = String.copyValueOf(data);
-        return NewTEL;
+
+        String tel;
+        tel = "(" + TEL[i + 1].substring(0, 3) + ") "
+                + TEL[i + 1].substring(3, 6) + "-"
+                + TEL[i + 1].substring(6, 10);
+        return tel;
     }
 }
